c04fb2a8101edac1a5e2c74022ea8ab100e8d70b
Martinique <me@martidolce.com>
2020-11-18 08:01:56 -0500

    commit tree 70385ce2cb2a0cc9b8fd9e633142b0eb4dfa81b7

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,21 @@
 # frozen_string_literal: true
 
-# Author: Martinique Dolce
+# brand: Martinique Dolce
 # Course: Flatiron School 2020, November 9 - 20201, April 2021
 # Contact: me@martidolce.com | https://modis.martidolce.com
 #
-#
+# shoe.rb
+
+class Shoe
+  attr_reader :brand
+  attr_accessor :material, :size, :color, :condition
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble
+    @condition = 'new'
+    puts 'Your shoe is as good as new!'
+  end
+end
